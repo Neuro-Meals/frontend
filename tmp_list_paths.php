53325e511ce06ac751ccecb2c0a6aae69dd465ad
raydanielg <airezra2+1@gmail.com>
2026-07-06 19:02:30 +0300

    commit

--- tmp_list_paths.php
+++
@@ -1,5 +0,0 @@
-<?php
-$j = json_decode(file_get_contents('f:\Nitromeals\tmp_openapi.json'), true);
-foreach ($j['paths'] as $p => $d) {
-    echo $p . PHP_EOL;
-}
--- tmp_parse_openapi.php
+++
@@ -1,8 +0,0 @@
-<?php
-$json = json_decode(file_get_contents('f:\\Nitromeals\\tmp_openapi.json'), true);
-$schema = $json['paths']['/auth/register']['post']['requestBody']['content']['application/json']['schema'] ?? null;
-if ($schema) {
-    echo json_encode($schema, JSON_PRETTY_PRINT);
-} else {
-    echo "Schema not found\n";
-}
