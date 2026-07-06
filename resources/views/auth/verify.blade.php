f7dd2fe8dad2d64658c74cea29150b5761a3b3bf
raydanielg <airezra2+1@gmail.com>
2026-07-07 01:22:29 +0300

    commit

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -74,7 +74,7 @@
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
-                    <span x-text="loadingVerify ? pleaseWait : 'Verify Email'"></span>
+                    <span x-text="loadingVerify ? pleaseWait : verifyText"></span>
                 </button>
             </form>
 
@@ -88,7 +88,7 @@
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
-                    <span x-text="loadingResend ? pleaseWait : 'Resend OTP'"></span>
+                    <span x-text="loadingResend ? pleaseWait : resendText"></span>
                 </button>
             </form>
         </div>
